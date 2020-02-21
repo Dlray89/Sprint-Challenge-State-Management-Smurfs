4d57f920afc8587f7598575e3f82397ec2ca3bbd
dlray89 <dlrayjr89@gmail.com>
2020-02-21 14:13:21 -0500

    minor styling

--- smurfs/src/components/smurfList.js
+++ smurfs/src/components/smurfList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import Logo from "../images/thesmurfslogo.png"
+import "./smurfs.css";
 
 const smurfList = props => {
   return (
     <div>
      <img src={Logo} alt="smurfs logo" width="200" style={{ margin: "0 auto"}}/>
       {props.Smurfs.map(smurf => (
-        <div key={smurf.id}>
+        <div className="card" key={smurf.id}>
 
-          <p  style={{ padding: "1%", borderRadius: "30px",border: "solid 2px #86a8e", background: "linear-gradient(to bottom, #7f7fd5, #86a8e7, #91eae4)", width: "40%", margin: "2% auto"}}>
+          <p style={{padding: "1%", borderRadius: "30px",border: "solid 2px #86a8e", background: "linear-gradient(to bottom, #7f7fd5, #86a8e7, #91eae4)", width: "25%", margin: "2% auto"}}>
             Name: {smurf.name} <br />
             age:{smurf.age} <br />
             Height:{smurf.height}
